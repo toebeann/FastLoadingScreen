d737bf5d3d51cdd17d536a73f2b63d9a39d6ce0c
Tobey Blaber <tobey@straitjacket.software>
2021-05-20 00:25:26 +0100

    Initial commit

---
+++ FastLoadingScreen/Main.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using QModManager.API.ModLoading;
+
+namespace Straitjacket.Subnautica.Mods.FastLoadingScreen
+{
+    [QModCore]
+    public static class Main
+    {
+        [QModPatch, Obsolete("Should not be used!", true)]
+        public static void Patch()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "FastLoadingScreen");
+        }
+    }
+}
---
+++ FastLoadingScreen/Patches/SaveLoadManagerPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System;
+
+namespace Straitjacket.Subnautica.Mods.FastLoadingScreen.Patches
+{
+    internal static class SaveLoadManagerPatch
+    {
+        [HarmonyPatch(typeof(SaveLoadManager), nameof(SaveLoadManager.LoadAsync), argumentTypes: new Type[0])]
+        [HarmonyPostfix]
+        public static void LoadAsyncPostfix()
+        {
+
+        }
+    }
+}
